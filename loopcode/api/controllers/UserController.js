928ab3ecccdd185257d21fa020c5c34f9ee6a617
Arjuna Noorsanto <awebgo.net@gmail.com>
2015-02-28 02:38:39 +0100

    mirror from live

--- loopcode/api/controllers/UserController.js
+++ loopcode/api/controllers/UserController.js
@@ -5,38 +5,55 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
-module.exports = {
+module.exports =
+{
 
-  /**
-   * `UserController.login()`
-   */
-  login: function (req, res) {
-    return res.login({
-      successRedirect: '/member'
-    });
-  },
+    /**
+     * `UserController.login()`
+     */
+    login : function(req, res)
+    {
+        return res.login(
+        {
+            successRedirect : '/member'
+        });
+    },
 
+    /**
+     * `UserController.logout()`
+     */
+    logout : function(req, res)
+    {
+        req.logout();
+        return res.redirect('/');
+        //res.ok('Logged out successfully.');
+    },
 
-  /**
-   * `UserController.logout()`
-   */
-  logout: function (req, res) {
-    req.logout();
-    return res.redirect('/'); //res.ok('Logged out successfully.');
-  },
+    /**
+     * `UserController.signup()`
+     */
+    signup : function(req, res)
+    {
+        User.create(req.params.all()).exec(function(err, user)
+        {
+            if (err)
+                return res.negotiate(err);
+            req.login(user, function(err)
+            {
+                if (err)
+                    return res.negotiate(err);
+                return res.redirect('/login');
+            });
+        });
+    },
 
-
-  /**
-   * `UserController.signup()`
-   */
-  signup: function (req, res) {
-    User.create(req.params.all()).exec(function (err, user) {
-      if (err) return res.negotiate(err);
-      req.login(user, function (err){
-        if (err) return res.negotiate(err);
-        return res.redirect('/login');
-      });
-    });
-  }
+    destroy : function(req, res)
+    {
+        User.destroy(req.params.all()).exec(function(err, user)
+        {
+            if (err)
+                return res.redirect('/login');
+        });
+    }
 };
 
