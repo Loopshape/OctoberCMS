f1f19c63f488ef8d2ab3c80d4fcd77e46a80d834
Arjuna Noorsanto <awebgo.net@gmail.com>
2015-02-28 22:42:42 +0100

    mirror from live-server

--- loopcode/api/controllers/AuthController.js
+++ loopcode/api/controllers/AuthController.js
@@ -1,47 +1,88 @@
 /**
- * AuthController
+ * AuthController.js
  *
- * @module		:: Controller
- * @description	:: Contains logic for handling auth requests.
+ * @description ::
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
 var passport = require('passport');
 
-module.exports =
-{
+module.exports = {
 
-    process : function(req, res)
-    {
-        passport.authenticate(
-        'local',
-        function(err, user, info)
-        {
-        if ((err) || (!user))
-        {
-        res.redirect('/login');
-        return;
-        }
-        // use passport to log in the user using a local method
-        req.logIn(
-        user,
-        function(err)
-        {
-        if (err)
-        {
-        res.redirect('/login');
-        return;
-        }
-        res.redirect('/');
-        return;
-        }
-        );
-        }
-        )(req, res);
-    },
+  index: function(req, res) {
+    res.view('/member');
+  },
 
-    logout : function(req, res)
-    {
-        req.logout();
-        res.redirect('/');
-    }
-}; 
+  logout: function(req, res) {
+    req.logout();
+    res.redirect('/');
+  },
+
+  // http://developer.github.com/v3/
+  // http://developer.github.com/v3/oauth/#scopes
+  github: function(req, res) {
+    passport.authenticate('github', { failureRedirect: '/' }, function(err, user) {
+      req.logIn(user, function(err) {
+        if (err) {
+          console.log(err);
+          res.view('500');
+          return;
+        }
+
+        res.redirect('/member');
+        return;
+      });
+    })(req, res);
+  },
+
+  // https://developers.facebook.com/docs/
+  // https://developers.facebook.com/docs/reference/login/
+  facebook: function(req, res) {
+    passport.authenticate('facebook', { failureRedirect: '/', scope: ['email'] }, function(err, user) {
+      req.logIn(user, function(err) {
+        if (err) {
+          console.log(err);
+          res.view('500');
+          return;
+        }
+
+        res.redirect('/member');
+        return;
+      });
+    })(req, res);
+  },
+
+  // https://developers.google.com/
+  // https://developers.google.com/accounts/docs/OAuth2Login#scope-param
+  google: function(req, res) {
+    passport.authenticate('google', { failureRedirect: '/', scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read'] }, function(err, user) {
+      req.logIn(user, function(err) {
+        if (err) {
+          console.log(err);
+          res.view('500');
+          return;
+        }
+
+        res.redirect('/member');
+        return;
+      });
+    })(req, res);
+  },
+
+  // https://apps.twitter.com/
+  // https://apps.twitter.com/app/new
+  twitter: function(req, res) {
+    passport.authenticate('twitter', { failureRedirect: '/' }, function(err, user) {
+      req.logIn(user, function(err) {
+        if (err) {
+          console.log(err);
+          res.view('500');
+          return;
+        }
+
+        res.redirect('/member');
+        return;
+      });
+    })(req, res);
+  }
+};
--- loopcode/api/models/User.js
+++ loopcode/api/models/User.js
@@ -1,82 +1,20 @@
 /**
- * User
+ * User.js
  *
- * @module   :: Model
- * @description :: A short summary of how this model works and what it represents.
- *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
  */
-var bcrypt = require('bcrypt');
 
-module.exports =
-{
+module.exports = {
 
-    schema : true,
+  adapter: 'mysql',
 
-    autoPK : false,
-    autoCreatedAt : false,
-    autoUpdatedAt : false,
-
-    table : 'user',
-    attributes :
-    {
-        id :
-        {
-            type : 'integer',
-            index : true,
-            required : true,
-            unique : true
-        },
-        username :
-        {
-            type : 'string',
-            required : true,
-            unique : true
-        },
-        password :
-        {
-            type : 'string',
-            minLength : 4,
-            required : true
-        },
-        email :
-        {
-            type : 'email',
-            required : true,
-            unique : true
-        },
-        toJSON : function()
-        {
-            var obj = this.toObject();
-            delete obj.password;
-            return obj;
-        }
-    },
-
-    // Lifecycle Callbacks
-    /*
-     beforeCreate : function(values, next)
-     {
-     next();
-     },
-     */
-    beforeCreate : function(user, cb)
-    {
-        bcrypt.genSalt(10, function(err, salt)
-        {
-            bcrypt.hash(user.password, salt, function(err, hash)
-            {
-                if (err)
-                {
-                    console.log(err);
-                    cb(err);
-                }
-                else
-                {
-                    user.password = hash;
-                    console.log(hash);
-                    cb(null, user);
-                }
-            });
-        });
-    },
+  attributes: {
+    provider: 'STRING',
+    uid: 'STRING',
+    name: 'STRING',
+    email: 'STRING',
+    firstname: 'STRING',
+    lastname: 'STRING'
+  }
 };
