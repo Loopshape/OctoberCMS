cb560afcebab499528bc7868b8da3cd04d7dd62f
Arjuna Noorsanto <awebgo.net@gmail.com>
2015-02-28 02:10:44 +0100

    mirror from live

--- loopcode/examples/untrustedClient.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  console.log(req.method + ' ' + req.url);
-  res.end(req.method + ' ' + req.url);
-}).listen(1339, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1339');
